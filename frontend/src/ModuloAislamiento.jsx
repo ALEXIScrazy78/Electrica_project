ecba0d078d16e055c2fdaad466a8d12000189e28
ALEXIScrazy78 <alexisdelpiero3034@gmail.com>
2026-07-01 18:03:26 -0500

    wsdds

--- frontend/src/ModuloAislamiento.jsx
+++ frontend/src/ModuloAislamiento.jsx
@@ -26,7 +26,7 @@ export default function ModuloAislamiento() {
   const handleCalculate = async () => {
     setError(null);
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/calcular-aislamiento', {
+      const response = await fetch('https://electrica-project.onrender.com/api/calcular-aislamiento', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
